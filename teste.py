7e366d335ae3580722b06e7416a8b712573a37a7
Ulisses Ribeiro <Ulisses.wrk@gmail.com>
2025-04-02 20:12:25 -0300

    Alterecao feita com sucesso

--- teste.py
+++ teste.py
@@ -1,14 +1,12 @@
 <<<<<<< HEAD
-idade = int(input('Digite a sua idade: '))
 
-
-
-print('Sua idade é', idade)
 
 
 =======
-
-
+idade = int(input('Digite a sua idade: '))
 nome = input('Digite o seu nome: ')
 print('Hello world, 2')
+print('Sua idade é', idade)
+
+
 >>>>>>> modificacao_print
